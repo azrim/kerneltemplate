eda5359f3e7d3d5e0e8157a7d70422bb42a39186
Azrim <mirzaspc@gmail.com>
2020-04-18 01:26:56 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Hentai kernel by azrim
+kernel.string=SiLonT kernel by azrim89
 do.devicecheck=1
 do.modules=0
 do.systemless=0
