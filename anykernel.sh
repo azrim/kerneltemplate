e9f810d5568046cfc4d46ed37a3fa2f3bae952c3
Azrim <mirzaspc@gmail.com>
2020-04-25 01:15:40 +0700

    Add files via upload

--- anykernel.sh
+++ anykernel.sh
@@ -1,13 +1,13 @@
+
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SiLonT kernel by azrim89
-do.devicecheck=1
-do.modules=0
-do.systemless=0
+kernel.string=SiLonT kranul by Azrim
+do.devicecheck=0
+do.modules=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=ginkgo
@@ -38,6 +38,25 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 ## AnyKernel install
 dump_boot;
 
+
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.gz
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+fi;
+
+
 write_boot;
 ## end install
+
 
