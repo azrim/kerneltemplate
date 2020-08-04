b5bb1af5990d37db672f451a228565aefe8e0206
azrim <mirzaspc@gmail.com>
2020-08-04 22:28:09 +0700

    update

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SiLonT kernel by azrim89
+kernel.string=Kernel by azrim @ github
 do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=ginkgo
+device.name2=willow
 device.name3=
 device.name4=
 device.name5=
@@ -19,25 +20,21 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=boot;
 is_slot_device=0;
-ramdisk_compression=auto;
+ramdisk_compression=none;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
 ## AnyKernel install
-dump_boot;
+#dump_boot;
+split_boot;
 
-write_boot;
+#write_boot;
+flash_boot;
+flash_dtbo;
 ## end install
 
